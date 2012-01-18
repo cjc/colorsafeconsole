cae235ba3f93dd02e8329970f0d34b4542bd0274
cjc <cjc@cjc.id.au>
2012-01-19 00:05:23 +1000

    Only require colors.js on func execute to prevent incorrect usage

--- ttycolors.js
+++ ttycolors.js
@@ -1,6 +1,6 @@
-var colors = require('colors')
-
 module.exports = function(outstream,override) { 
+  var colors = require('colors')
+  outstream = outstream || process.stdout
   if (!override && outstream && outstream._type != 'tty') {
     colors.mode = 'none'
   }
